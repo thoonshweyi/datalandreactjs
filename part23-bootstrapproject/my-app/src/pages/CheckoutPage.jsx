8aabdc33bcc37a1879afad75ae2ebd05cb210440
thoonshweyikyaw <thoonshweyikyaw@gmail.com>
2025-09-26 22:48:05 +0630

    react tostify

--- part23-bootstrapproject/my-app/src/pages/CheckoutPage.jsx
+++ part23-bootstrapproject/my-app/src/pages/CheckoutPage.jsx
@@ -2,14 +2,27 @@ import React,{useState,useEffect} from "react";
 import {useNavigate} from "react-router";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {faArrowLeft,faBoxOpen,faStar,faTrash,faSpinner,faExclamation,faTag,faShoppingCart,faTruck,faCreditCard,faMoneyBill} from "@fortawesome/free-solid-svg-icons"
- 
+import { ToastContainer, toast } from 'react-toastify';
+
 import banner4 from "../assets/img/banner/banner4.jpg"
 
 
 const CheckoutPage = ()=>{
      const navigate = useNavigate();
      const [carts,setCarts] = useState([]);
+     const [form,setForm] = useState({
+          fullname: "",
+          email: "",
+          phone:"",
+          address:"",
+          city:"",
+          zip:"",
+          country:""
+     });
+     const [payment,setPayment] = useState('card');
+
      const [loading,setLoading] = useState(true);
+
 
      useEffect(()=>{
           const getcarts = JSON.parse(localStorage.getItem('cart')) || []; // abccart // companynamecart
@@ -17,30 +30,35 @@ const CheckoutPage = ()=>{
 
      },[]);
 
-     const removeHandler = (productid)=>{
-          const updatecart = carts.filter(cart => cart.id != productid);
-          setCarts(updatecart);
-          localStorage.setItem("cart", JSON.stringify(updatecart));
+     const subtotal = carts.reduce((sum,cart)=>sum + (cart.price * cart.qty) ,0);
+     const shipping = +subtotal < 300 ? 10 : 0 ;
+     const tax = +(subtotal * 0.1).toFixed(2);
+     const grandtotal = subtotal + shipping + tax; 
+
+     const changeHandler = (e)=>{
+          setForm({...form,[e.target.name]:e.target.value});
      }
 
-     const clearHandler = (id)=>{
-          setCarts([]);
+     const placeorderHandler = ()=>{
+          if(!form.fullname || !form.email || !form.phone || !form.address){
+               console.log("No form data");
+               toast.error("Please fill in all require info!")
+
+               return;
+          }
           localStorage.removeItem("cart");
-     }
+          toast.success(`Order placed successfully! Total: $ ${grandtotal.toFixed(2)} | Payment: ${payment}`)
 
-     // if(carts.length == 0){
-     //      return (
-     //           <div className="container text-center py-3">
-     //                <h3>Your cart is empty</h3>
-     //                <button type="button" className="btn btn-outline-secondary mb-4" onClick={()=>navigate(-1)}><FontAwesomeIcon icon={faArrowLeft} />Back To Shop</button>
-     //           </div>
-     //      )
-     // }
 
-     const total = carts.reduce((prev,next)=>prev + next.price * (next.qty || 1),0);
-     
+          setTimeout(()=>{
+               navigate("/");
+          },3000); // redirect to index after 3s
+     };
+
      return (
           <main className="bg-light">
+               <ToastContainer />
+
                {/* Banner */}
                <section className="text-center  text-light d-flex justifiy-content-center align-items-center" style={{
                     minHeight: "70vh",
@@ -78,37 +96,37 @@ const CheckoutPage = ()=>{
                                              <div className="row g-3">
                                                   <div className="col-md-6">
                                                        <label htmlFor="fullname" className="form-label">Full Name *</label>
-                                                       <input type="text" name="fullname" id="fullname" className="form-control" required/>
+                                                       <input type="text" name="fullname" id="fullname" className="form-control" value={form.fullname} onChange={changeHandler} required/>
                                                   </div>
 
                                                   <div className="col-md-6">
                                                        <label htmlFor="email" className="form-label">Email *</label>
-                                                       <input type="email" name="email" id="email" className="form-control" required/>
+                                                       <input type="email" name="email" id="email" className="form-control" value={form.email} onChange={changeHandler} required/>
                                                   </div>
 
                                                   <div className="col-md-6">
                                                        <label htmlFor="phone" className="form-label">Phone *</label>
-                                                       <input type="text" name="phone" id="phone" className="form-control" required/>
+                                                       <input type="text" name="phone" id="phone" className="form-control" value={form.phone} onChange={changeHandler} required/>
                                                   </div>
 
                                                   <div className="col-md-12">
                                                        <label htmlFor="address" className="form-label">Address * </label>
-                                                       <input type="text" name="address" id="address" className="form-control" required/>
+                                                       <input type="text" name="address" id="address" className="form-control" value={form.address} onChange={changeHandler} required/>
                                                   </div>
                                                   
                                                   <div className="col-md-4">
                                                        <label htmlFor="city" className="form-label">City </label>
-                                                       <input type="text" name="city" id="city" className="form-control"/>
+                                                       <input type="text" name="city" id="city" className="form-control" value={form.city} onChange={changeHandler}/>
                                                   </div>
 
                                                   <div className="col-md-4">
                                                        <label htmlFor="zip" className="form-label">Zip Code </label>
-                                                       <input type="text" name="zip" id="zip" className="form-control"/>
+                                                       <input type="text" name="zip" id="zip" className="form-control" value={form.zip} onChange={changeHandler}/>
                                                   </div>
 
                                                   <div className="col-md-4">
                                                        <label htmlFor="country" className="form-label">Country </label>
-                                                       <input type="text" name="country" id="country" className="form-control"/>
+                                                       <input type="text" name="country" id="country" className="form-control" value={form.country} onChange={changeHandler}/>
                                                   </div>
                                                   
                                              </div>
@@ -121,17 +139,17 @@ const CheckoutPage = ()=>{
                                    <div className="card-body">
                                         <h5><FontAwesomeIcon icon={faCreditCard} className="me-2"/> Payment Method</h5>
                                         <div className="form-check">
-                                             <input type="radio" name="payment" id="cd" className="form-check-input" value="1" /> 
+                                             <input type="radio" name="payment" id="cd" className="form-check-input" value="card" onChange={(e)=>setPayment(e.target.value)}/> 
                                              <label htmlFor="cd" className="form-check-label">Credit / Debit Card</label>
                                         </div>
 
                                         <div className="form-check">
-                                             <input type="radio" name="payment" id="bt" className="form-check-input" value="2"/> 
+                                             <input type="radio" name="payment" id="bt" className="form-check-input" value="bank" onChange={(e)=>setPayment(e.target.value)}/> 
                                              <label htmlFor="bt" className="form-check-label">Bank Transfer</label>
                                         </div>
 
                                         <div className="form-check">
-                                             <input type="radio" name="payment" id="cod" className="form-check-input" value="3"/> 
+                                             <input type="radio" name="payment" id="cod" className="form-check-input" value="cod" onChange={(e)=>setPayment(e.target.value)}/> 
                                              <label htmlFor="cod" className="form-check-label">Cash on Delivery</label>
                                         </div>
                                    </div>
@@ -143,36 +161,40 @@ const CheckoutPage = ()=>{
                                         <h5 className="3">Order Summary</h5>
 
                                         <ul className="list-group mb-3">
-                                             <li className="list-group-item d-flex justify-content-between">
-                                                  <div>
-                                                       <h6>Chair</h6>
-                                                       <small className="text-muted">Qty: 1</small>
-                                                  </div>
-                                                  <span>$ 358.00</span>
-                                             </li>
+                                             {
+                                                  carts.map(cart=>(
+                                                       <li key={cart.id} className="list-group-item d-flex justify-content-between">
+                                                            <div>
+                                                                 <h6>{cart.title}</h6>
+                                                                 <small className="text-muted">Qty: {cart.qty}</small>
+                                                            </div>
+                                                            <span>$ {(cart.price * cart.qty.toFixed(2)) }</span>
+                                                       </li>
+                                                  ))
+                                             }
 
                                              <li className="list-group-item d-flex justify-content-between">
                                                   <strong>Subtotal</strong>
-                                                  <span>$ 358.00</span>
+                                                  <span>$ {subtotal.toFixed(2)}</span>
                                              </li>
 
                                              <li className="list-group-item d-flex justify-content-between">
                                                   <strong>Shipping</strong>
-                                                  <span>$ 10.00</span>
+                                                  <span>{shipping == 0 ? "Free" : `$ ${shipping}`}</span>
                                              </li>
 
                                              <li className="list-group-item d-flex justify-content-between">
                                                   <strong>Tax (10%)</strong>
-                                                  <span>$ 36.00</span>
+                                                  <span>$ {tax.toFixed(2)}</span>
                                              </li>
 
                                              <li className="list-group-item d-flex justify-content-between">
                                                   <strong>Grand Total</strong>
-                                                  <span>$ 396.00</span>
+                                                  <span>$ {grandtotal.toFixed(2)}</span>
                                              </li>
                                         </ul>
 
-                                        <button type="button" className="w-100 btn btn-dark"> <FontAwesomeIcon icon={faMoneyBill} className="me-2" /> Place Order</button>
+                                        <button type="button" className="w-100 btn btn-dark" onClick={placeorderHandler}> <FontAwesomeIcon icon={faMoneyBill} className="me-2" /> Place Order</button>
                                    </div>
                               </div>
                          </div>
@@ -187,4 +209,7 @@ const CheckoutPage = ()=>{
 export default CheckoutPage;
 
 {/* <FontAwesomeIcon icon="fa-solid fa-spinner" spin size="3x"/> */}
-    
+
+// =>react tostify
+//        https://www.npmjs.com/package/react-toastify
+//        https://fkhadra.github.io/react-toastify/introduction
